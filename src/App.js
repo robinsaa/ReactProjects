1b7e7934b5cc2c9c244d0c0ad31aa332db9dce64
Aaron Robins <aaronrobins@Aarons-MacBook-Pro.local>
2022-06-15 01:26:06 +1000

    router v6 practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Entrypoint from "./components/Entrypoint";
+import Home from "./components/Home";
 import './App.css';
+import Subpage1 from './components/Subpage1';
+import Subpage2 from './components/Subpage2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Entrypoint/>}/>
+        <Route path="/apps" element = {<Navigate replace to="/courses"/>}/>
+        <Route path="/home" element = {<Home />}>
+          <Route path="Subpage1" element = {<Subpage1 />}/>
+          <Route path="Subpage2" element = {<Subpage2 />}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
